e9363e9ce4c36ac0b351e57ca866f45b0da7087e
Ricky <rickdhanota@gmail.com>
2020-12-30 22:37:25 -0800

    added another method

--- ninja_oop.js
+++ ninja_oop.js
@@ -1,8 +1,20 @@
 class Ninja{
-    constructor(name, health, speed = 3, strength = 3){
+    constructor(name, health = 1, speed = 3, strength = 3){
         this.name = name;
         this.health = health;
         this.speed = speed;
         this.strength = strength;
     }
+
+    sayName(){
+        console.log(`This Ninja's name is ${this.name}`);
+        return this;
+    }
+
+    showStats(){
+        console.log(`This Ninja is ${this.name}, and this Ninja's stats are; Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
+        return this;
+    }
 }
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
